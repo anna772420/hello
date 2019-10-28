e37b70ce54cb10c7f17bc5a7b1774532e318258c
anna772420 <anna772420@gmail.com>
2019-10-28 22:19:47 +0800

    modify h1

--- scientificGirl.php
+++ scientificGirl.php
@@ -14,7 +14,7 @@
 </head>
 <body>
 	<div class="phototitle">
-		<font color ="#FFF0F5"><h1 align="center">精彩劇照與花絮</h1></font>
+		<font color ="#FFF0F5"><h1 align="center">精彩劇照與花絮美照</h1></font>
 	</div>
 	
 	<div id="carousel" class="carousel slide" data-ride="carousel">
